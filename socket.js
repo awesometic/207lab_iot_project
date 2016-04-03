d9489ba919cd69bc34480281e5c9d7bdda5567e4
awesometic <secugyu@gmail.com>
2016-04-03 15:50:23 +0900

    Update: modifying code to test the module socket.io

--- socket.js
+++ socket.js
@@ -8,13 +8,23 @@ var io = require("socket.io").listen(2070);
 // We'll communicate using json object
 // Reference: http://stackoverflow.com/questions/25615313/javascript-byte-array-to-json-and-back
 
-// Making left stuffs after android app that can communicate via socket is made
-io.sockets.on("connection", function(socket) {
-    socket.on("call", function(data) {
-        console.log(data);
-        socket.emit("answer", "hi");
+// There's java library that is made for socket.io
+// http://socket.io/blog/native-socket-io-and-android/
+// https://github.com/socketio/socket.io-client-java
+
+// Making left stuffs after android app that can communicate via socket that is made
+io.on("connection", function(socket) {
+    socket.emit("an event", {
+        MyNameIs: "Node.js"
     });
 
+    socket.on("other event", function(data) {
+        console.log(data);
+    });
+
+    socket.on("disconnect", function() {
+        io.emit("user disconnected");
+    })
 });
 
 module.exports = io;
