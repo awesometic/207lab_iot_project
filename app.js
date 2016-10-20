043f2419ce3f0731fa45909bfd563a69d949e347
awesometic <secugyu@gmail.com>
2016-10-20 17:38:16 +0900

    server: revert the copying style and javascript files back to be affected by admin-lte versioning

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/admin-lte')));
 
 app.use(session({
     secret: "keyboard cat",
