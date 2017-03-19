acaaebfce96277790a7808dfd29478d7c754cae9
awesometic <secugyu@gmail.com>
2017-03-19 16:54:11 +0900

    fix displaying time duration

--- public/commonJqueryFunctions.js
+++ public/commonJqueryFunctions.js
@@ -6,34 +6,22 @@ var commonFunc = function() {
                 .replace(/\"\[/g, '\[')
                 .replace(/\]\"/g, '\]'));
         },
-        // http://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript
+        // http://stackoverflow.com/questions/9763441/milliseconds-to-time-in-javascript
         msecToTimeDuration : function(durationMsec) {
-            var milliseconds = parseInt((durationMsec % 1000) / 100)
-                , seconds = parseInt((durationMsec / 1000) % 60)
-                , minutes = parseInt((durationMsec / (1000 * 60)) % 60)
-                , hours = parseInt((durationMsec / (1000 * 60 * 60)) % 24);
-
-            hours = (hours < 10) ? "0" + hours : hours;
-            minutes = (minutes < 10) ? "0" + minutes : minutes;
-            seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-            return hours + ":" + minutes + ":" + seconds;//+ "." + milliseconds;
-        },
-        msecToTimeDurationSimple : function(durationMsec) {
-            var seconds = (millisec / 1000).toFixed(1);
-            var minutes = (millisec / (1000 * 60)).toFixed(1);
-            var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-            var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
-            if (seconds < 60) {
-                return seconds + " 초";
-            } else if (minutes < 60) {
-                return minutes + " 분";
-            } else if (hours < 24) {
-                return hours + " 시간";
-            } else {
-                return days + " 일"
+            // Pad to 2 or 3 digits, default is 2
+            function pad(n, z) {
+                z = z || 2;
+                return ('00' + n).slice(-z);
             }
+
+            var ms = durationMsec % 1000;
+            durationMsec = (durationMsec - ms) / 1000;
+            var secs = durationMsec % 60;
+            durationMsec = (durationMsec - secs) / 60;
+            var mins = durationMsec % 60;
+            var hrs = (durationMsec - mins) / 60;
+
+            return ((hrs < 10) ? pad(hrs) : hrs) + ':' + pad(mins) + ':' + pad(secs);// + '.' + pad(ms, 3);
         }
     }
 }();
